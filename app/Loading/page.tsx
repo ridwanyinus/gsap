20ddb1755f7c9d01fdcbb66a01bf489c2eefbcbf
Ridwan <ridwanyinusa15@gmail.com>
2024-08-28 10:02:20 +0100

    chore: add new heading

--- app/Loading/page.tsx
+++ app/Loading/page.tsx
@@ -5,7 +5,9 @@ const page = () => {
     <div>
       <Loading />
       <div className="bg-black h-screen flex justify-center items-center">
-        <h1 className="text-8xl text-white text-center">Hello My Nigga!!!</h1>
+        <h1 className="text-6xl text-white text-center">
+          The first rule of <code>Fight Club</code> is: you do not talk about <code>Fight Club</code>.
+        </h1>
       </div>
     </div>
   );
